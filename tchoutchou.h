1c8096e34a9647f431d85d6eb70ab27bfb685996
kevin <kevin.progc@gmail.com>
2023-11-22 18:55:36 +0100

    Refonte totale

--- tchoutchou.h
+++ tchoutchou.h
@@ -9,75 +9,46 @@
 
 #define MIN_SPEED 0.005f
 
-namespace tchoutchou
+namespace tch
 {
-    struct Way
-    {
-        std::vector<glm::vec3> points;
-    };
-
     struct Bogy
     {
-        glm::vec3 pos;
-        unsigned int before;
-        unsigned int after;
-    };
-
-    struct Car
-    {
-        float interval[2];
+        float posInit;
         glm::vec3 pos;
         float angle;
     };
 
     struct Vehicle
     {
-        std::vector<float> posInitBogies;
         std::vector<Bogy> bogies;
-        std::vector<Car> cars;
-        float mass;                         //t
-        float maxPower;                     //kW
-        float maxSpeed;                     //km/h
-        float throttle=0.0f;                //[-1.0 , 1.0]
-        float speed=0.0f;                   //m/frame
-        unsigned int indexWay;
-        unsigned int indexPointFirstBogy;
-        int inverter=0;
-        bool forth;
     };
 
-    struct Demisphere
+    struct Point
     {
-        glm::vec3 center;
-        float radius;
-        glm::vec3 direction;
+        glm::vec3 pos;
+        int indexAfter = -1;
+        int indexBefore = -1;
     };
 
-    struct Segment
-    {
-        glm::vec3 v1;
-        glm::vec3 v2;
-    };
-
-    class FollowRails
+    class World
     {
         private :
 
-        std::vector<Way> ways;
-        //std::vector<Vehicle> vehicles;
-        unsigned int timeFrame;             //milliseconds
-
-        bool InitVehicleBogies(Vehicle *vehicle);
-        bool detectOverflowSeg(const glm::vec3 pointBefore,const glm::vec3 pointAfter,const glm::vec3 posBogy,float *overflow);
-        void moveHeadBogy(Vehicle *vehicle,bool *forth);
-        void moveOthersBogies(Vehicle *vehicle,const bool forth);
-        void vehicleCinetic(Vehicle *vehicle,bool *forth);
+        std::vector<Point> points;
+        std::vector<Vehicle> vehicles;
 
         public :
 
-        void Init(std::vector<Way> &appWays,std::vector<Vehicle> &appVehicles,const unsigned int appTimeFrame);
-        void moveVehicle(Vehicle *vehicle);
-    };
+        void addPoint(Point p)
+        {
+            points.push_back(p);
+        }
 
-    void betweenDemisphereSegment(Demisphere *demiSphere,Segment *seg,std::vector<glm::vec3> &intersections);
+        void InitPoints(std::vector<Point> array)
+        {
+            points = array;
+        }
+
+        void addVehicle(std::vector<float> posBogies, const size_t indexPoint);
+    };
 }
